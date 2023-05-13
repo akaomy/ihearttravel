3e764eda201bb867e28925cfd4c4a861a2c12c39
akaomy <annakozlova92@gmail.com>
2023-05-13 13:32:03 -0700

    add responsivenes to three different width screen sizes

--- clientapp/src/components/todo/TodoBoard.js
+++ clientapp/src/components/todo/TodoBoard.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import TodoCard from './TodoCard';
+import Grid from '@mui/material/Grid';
+
 
 export default function TodoBoard () {
     return (
-        <>
-            <TodoCard />
-            <TodoCard />
-            <TodoCard />
-            <TodoCard />
-        </>
+        <Grid container spacing={4}>
+            <Grid item xs={12} sm={4} md={3}>
+                <TodoCard />
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <TodoCard />
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <TodoCard />
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <TodoCard />
+            </Grid>
+        </Grid>
     )
 }
