1637f86bcfae7e6b271d20b1b96527b9e944d550
akaomy <annakozlova92@gmail.com>
2023-06-03 06:14:23 -0700

    add initial map component

--- clientapp/src/components/map/Map.js
+++ clientapp/src/components/map/Map.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
+
+const containerStyle = {
+    width: '960px',
+    height: '630px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
 
 export default function Map() {
     return (
-        <>map goes here</>
+        <LoadScript
+            // temp
+            googleMapsApiKey='your api key here'
+        >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+      </LoadScript>
     );
 }
