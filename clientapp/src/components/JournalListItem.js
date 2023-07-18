dac3398bb4dd19455e523d21d19c28719b9c3450
akaomy <annakozlova92@gmail.com>
2023-07-18 13:26:08 -0700

    add create and delete journal buttons on JournalListView page

--- clientapp/src/components/JournalListItem.js
+++ clientapp/src/components/JournalListItem.js
@@ -2,24 +2,31 @@
 import React from 'react';
 import { ListItemButton, ListItemText, ImageListItem, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
+import CustomButton from './uiReusables/CustomButton';
+import { styled } from '@mui/material/styles';
 
 
- const JournalListItem = ({name}) => {
+const StyledContainer = styled(Container)(() => ({
+    display: 'flex',
+}));
 
-    return (
-        <Container maxWidth="md">
-            <ListItemButton component={Link} to={name}>
-                <ImageListItem>
-                    <img src='https://placehold.co/100x100' alt={name}/>
-                </ImageListItem>
-                <Container maxWidth="md">
-                    <ListItemText>
-                        {name}
-                    </ListItemText>
-                </Container>
-            </ListItemButton>
-        </Container>
-        );
+const JournalListItem = ({name}) => {
+
+return (
+    <StyledContainer maxWidth="md">
+        <ListItemButton component={Link} to={name}>
+            <ImageListItem>
+                <img src='https://placehold.co/100x100' alt={name}/>
+            </ImageListItem>
+            <Container maxWidth="md">
+                <ListItemText>
+                    {name}
+                </ListItemText>
+            </Container>
+        </ListItemButton>
+        <CustomButton btnText={'delete'}/>
+    </StyledContainer>
+    );
 }
 
 export default JournalListItem;
--- clientapp/src/components/JournalListView.js
+++ clientapp/src/components/JournalListView.js
@@ -1,19 +1,30 @@
 
 import React from 'react';
 import JournalListItem from './JournalListItem';
+import CustomButton from './uiReusables/CustomButton';
+import { Container } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
+
+const StyledContainer = styled(Container)(() => ({
+    display: 'flex',
+    justifyContent: 'space-around'
+}));
 
 const JournalListView  = ({data}) => {
     
     return (
-        <>
+        <Container>
+            <StyledContainer>
+                <CustomButton btnText={'+'}/>
+            </StyledContainer>
             {data.user.journals.map((i) => 
                 <JournalListItem 
                     key={i.journal_name}
                     name={i.journal_name}
                 />
             )}
-        </>
+        </Container>
     );
 }
 
