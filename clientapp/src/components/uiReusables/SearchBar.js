3a0d83b51e7e95d251fabfca63b8b71c5f15f257
akaomy <annakozlova92@gmail.com>
2023-07-26 18:00:30 -0700

    add custom button search for SearchBar component

--- clientapp/src/components/uiReusables/SearchBar.js
+++ clientapp/src/components/uiReusables/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import CustomButton from '../uiReusables/CustomButton';
 
 
 export default function SearchBar () {
     return (
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <CustomButton btnText={'search'}/>
+        </>
     );
 }
