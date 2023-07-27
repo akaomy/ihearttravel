9123fa9c8e62f99d631c83e54269e5c8209d7cc1
akaomy <annakozlova92@gmail.com>
2023-07-26 18:03:26 -0700

    Revert "add custom button search for SearchBar component"
    This reverts commit 3a0d83b51e7e95d251fabfca63b8b71c5f15f257.

--- clientapp/src/components/uiReusables/SearchBar.js
+++ clientapp/src/components/uiReusables/SearchBar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
-import CustomButton from '../uiReusables/CustomButton';
 
 
 export default function SearchBar () {
     return (
-        <>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <CustomButton btnText={'search'}/>
-        </>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     );
 }
