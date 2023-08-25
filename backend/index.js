ced8f27b11cdab2cc95769e2e1e15c7d7104a55d
akaomy <annakozlova92@gmail.com>
2023-08-24 20:09:50 -0700

    prep backend route that would serve for creating new journal

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,19 @@ app.get('/journals/:id', async function (req, res) {
     res.send("<pre>" + JSON.stringify(journal, undefined, 4) + "<pre/>");
 });
 
+// todo:
+// app.get('/journals/create', async function (req, res) {
+//     try {
+//         let journal = await models.Journal.create({
+//         //how to get this data from the front end form?
+//         });
+//         res.status(201).json(journal);
+//     } catch (err) {
+//             console.log(err);
+//             res.status(500).json({err: 'An error occured while creating new record'});
+//     }
+// });
+
 // app.get('/journal-map-places', async (req, res) => {
 //     const journalMapPlaces = await models.JournalMapPlaces.findAll();
 //     res.send(JSON.stringify(journalMapPlaces, undefined, 4));
