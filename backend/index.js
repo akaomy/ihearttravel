3fcec8c7f57f343d4a829160b086e0bc9faa36e2
akaomy <annakozlova92@gmail.com>
2023-12-22 10:32:06 -0800

    remove JSON formating on the backend, it causes error - not valid JSON

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.get('/', async (req, res) => {
 
 app.get('/journals', async (req, res) => {
     const journals = await models.Journal.findAll();
-    res.send("<pre>" + JSON.stringify(journals, undefined, 4) + "<pre/>");
+    res.send(JSON.stringify(journals, undefined, 4));
 });
 
 app.get('/journals/:id', async function (req, res) {
@@ -31,7 +31,7 @@ app.get('/journals/:id', async function (req, res) {
     if (!journal) {
         return res.sendStatus(404);
     }
-    res.send("<pre>" + JSON.stringify(journal, undefined, 4) + "<pre/>");
+    res.send(JSON.stringify(journal, undefined, 4));
 });
 
 // todo:
