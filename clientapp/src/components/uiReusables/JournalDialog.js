062ba4aa5661366688106785ad8f6a724fb4a9b6
akaomy <annakozlova92@gmail.com>
2024-02-24 22:14:53 -0800

    add close dialog once a journal is created

--- clientapp/src/components/uiReusables/JournalDialog.js
+++ clientapp/src/components/uiReusables/JournalDialog.js
@@ -12,6 +12,7 @@ import axios from 'axios';
 export default function JournalDialog() {
   const [open, setOpen] = React.useState(false);
   const [journalName, setJournalName] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('')
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -21,8 +22,16 @@ export default function JournalDialog() {
     setOpen(false);
   };
 
-
+  // TODO: Add validation for journal name (no empty strings, not more than 255 characters, strings and numbers only)
+  // TODO: close the dialog when the button 'create' is clicked
+  // TODO: show new list items in the list view (how to update the list items without reloading (visibly?) the page?)
   const handleCreate = () => {
+    // validate if the journal name is not empty
+    if (journalName.length === 0) {
+      setErrorMessage('Journal name cannot be empty');
+      return;
+    }
+
     const data = { JournalName: journalName };
     axios.post('http://localhost:4400/journals/create', data)
         .then(response => {
@@ -30,6 +39,7 @@ export default function JournalDialog() {
             // Handle success or navigation to another page
         })
         .catch(error => console.log('error ', error));
+      setOpen(false);
   };
 
 
@@ -48,8 +58,12 @@ export default function JournalDialog() {
             fullWidth
             variant="standard"
             value={journalName}
-            onChange={(e) => setJournalName(e.target.value)}
+            onChange={ (e) => {
+              setJournalName(e.target.value);
+              setErrorMessage('');
+            }}
           />
+        {errorMessage && <p style={{color:'red'}}>{errorMessage}</p>}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>{strings.buttonCancel}</Button>
